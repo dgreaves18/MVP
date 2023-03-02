705c4bb429831b103e1dc7e2e756b9bbe720e017
Daniel Greaves <dgreaves18@gmail.com>
2023-03-01 23:58:22 -0800

    Added route handling + db interaction

--- server/index.js
+++ server/index.js
@@ -2,20 +2,43 @@ const express = require('express');
 require('dotenv').config();
 const morgan = require('morgan');
 const path = require('path');
-// const router = require('./router');
+const cors = require('cors');
+const Players = require('./db.js');
 
 const app = express();
 const port = process.env.PORT;
 
 // MIDDLE WARE
-// app.use(cors());
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.set('views', path.join(__dirname, '../client/dist'));
-
-// app.use('/', router);
 app.use(express.static(path.join(__dirname, '../client/dist'))); // SERVE CLIENT FILES
+
+app.post('/player/data', (req, res) => {
+  // console.log(req);
+  // console.log(req.params);
+  console.log(req.body);
+  Players.find({full_name: `${req.body.full_name}`})
+    .then((data) => {
+      // console.log(data);
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      console.log('Error finding player', err);
+      res.sendStatus(500);
+    });
+});
+
+app.post('/player', (req, res) => {
+  Players.create(req.body)
+    .then(() => {
+      res.send('Success');
+    })
+    .catch((err) => {
+      res.send('Error', err);
+    });
+});
 
 app.listen(port);
 // eslint-disable-next-line no-console
